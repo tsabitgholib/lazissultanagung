5fa614137590b4138dff99ac80030bcd94d91e75
tsabitgholib <tsabitgholib@gmail.com>
2026-02-08 14:21:23 +0700

    Add createdAt and updatedAt fields to Agen during creation

--- src/main/java/com/lazis/lazissultanagung/service/AgenServiceImpl.java
+++ src/main/java/com/lazis/lazissultanagung/service/AgenServiceImpl.java
@@ -63,6 +63,8 @@ public class AgenServiceImpl implements AgenService {
         );
         
         agen.setTargetAmount(agenRequest.getTargetAmount());
+        agen.setCreatedAt(new Date());
+        agen.setUpdatedAt(new Date());
         
         return agenRepository.save(agen);
     }
