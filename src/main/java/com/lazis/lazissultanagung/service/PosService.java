3e67047f8be41ce99505b1aa37377a32212fbb42
tsabitgholib <tsabitgholib@gmail.com>
2026-02-14 22:28:12 +0700

    FIX

--- src/main/java/com/lazis/lazissultanagung/service/PosService.java
+++ src/main/java/com/lazis/lazissultanagung/service/PosService.java
@@ -1,23 +1,21 @@
 package com.lazis.lazissultanagung.service;
 
-import java.time.LocalDate;
 import com.lazis.lazissultanagung.dto.request.PosTransactionRequest;
+import com.lazis.lazissultanagung.dto.response.PosDashboardResponse;
 import com.lazis.lazissultanagung.dto.response.PosHistoryResponse;
 import com.lazis.lazissultanagung.dto.response.PosTransactionResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import com.lazis.lazissultanagung.dto.response.PosDashboardResponse;
-import org.springframework.web.multipart.MultipartFile;
-
 public interface PosService {
     PosTransactionResponse createPosTransaction(PosTransactionRequest request, Long agenId);
-    
+
     Page<PosHistoryResponse> getPosHistory(Long agenId, Long eventId, LocalDate startDate, LocalDate endDate, String category, String paymentMethod, String search, Pageable pageable);
-    java.util.List<PosHistoryResponse> getPosHistoryList(Long agenId, Long eventId, LocalDate startDate, LocalDate endDate, String category, String paymentMethod, String search);
+    List<PosHistoryResponse> getPosHistoryList(Long agenId, Long eventId, LocalDate startDate, LocalDate endDate, String category, String paymentMethod, String search);
 
     PosDashboardResponse getPosDashboard(Long agenId);
 
@@ -25,4 +23,3 @@ public interface PosService {
     void importTransactionsFromExcel(MultipartFile file, Long agenId);
     List<PosHistoryResponse> getDistinctDonaturPos(String search);
 }
-
